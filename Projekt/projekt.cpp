39429e98a84be986a57d08ff1646fd474fe2aa38
s30020-pj <146538548+s30020-pj@users.noreply.github.com>
2023-12-29 08:44:45 +0100

    Update projekt.cpp
    Changes in code structure + addition of straight ways

--- Projekt/projekt.cpp
+++ Projekt/projekt.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib> // for rand() and srand()
-#include <ctime> // for time()
+#include <cstdlib>
+#include <ctime> 
 
 using namespace std;
 
 int main() {
-    srand(time(0)); // seed for random number generator
-
+    
+    srand(time(0));
     int rows, cols;
     cout << "Enter number of rows: ";
     cin >> rows;
     cout << "Enter number of columns: ";
     cin >> cols;
 
+    // create table for random numbers
     vector<vector<int>> table(rows, vector<int>(cols));
 
+    // fill table with random numbers between 1 and 100
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
-            table[i][j] = rand() % 100; // random numbers between 0 and 99
+            table[i][j] = rand() % 100; 
         }
     }
 
@@ -26,6 +28,25 @@ int main() {
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    // vector containing possible ways through table
+    vector<vector<int>> ways(0, vector<int>(cols)); 
+    
+    //fill ways vector with one rows ways
+    for(int i = 0; i < rows; i++) {
+        ways.push_back(vector<int>());
+        for(int j = 0; j < cols; j++) {
+            ways[i].push_back(table[i][j]);
+        }
+    }
+    
+    // print the ways
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cout << ways[i][j] << " ";
         }
         cout << endl;
     }
